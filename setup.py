3389deb375e113374cc291dad7c5dc493e39cc25
SidUchil <siddharth.uchil@gmail.com>
2023-08-24 18:16:56 -0400

    Reverse Search Engine commits

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Endpoint",
+    version="0.0.1",
+    author="Siddharth Uchil",
+    author_email="siddharth.uchil@gmail.com",
+    packages=find_packages()
+)
